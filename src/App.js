cefa5d872eee92b6d75a3e5696c083c2acdc3b2f
kahyunnam <69875166+kahyunnam@users.noreply.github.com>
2021-06-19 00:17:44 -0400

    add search

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
 
 import './App.css';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchVlue] = useState('');
+  const [searchValue, setSearchValue] = useState('');
 
-  const getMovieRequest = async () => {
-    const url = "http://www.omdbapi.com/?s=star wars&apikey=ddacb17c"
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=ddacb17c`
     const response = await fetch(url)
     const responseJson = await response.json();
-    console.log(responseJson);
 
-    setMovies(responseJson.Search)
+    if (responseJson.Search) {
+      setMovies(responseJson.Search)
+    }
   }
 
   useEffect(() => {
-    getMovieRequest();
-  }, []);
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
   return (
     <div className='container-fluid movie-app'>
-      <div className="row">
-
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="movies" />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
         <MovieList movies={movies} />
