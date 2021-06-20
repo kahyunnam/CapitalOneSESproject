8344102737815930674daf7ea6d852c4cd67c761
kahyunnam <69875166+kahyunnam@users.noreply.github.com>
2021-06-20 14:26:31 -0400

    smol changes

--- src/App.js
+++ src/App.js
@@ -4,26 +4,36 @@ import './App.css';
 import MovieList from './components/MovieList';
 import MovieListHeading from './components/MovieListHeading';
 import SearchBox from './components/SearchBox';
+import styled from 'styled-components';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
 import './App.css';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
+  const [page, setPage] = useState(1);
 
-  const getMovieRequest = async (searchValue) => {
-    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=ddacb17c`
+  const nextPageButton = styled(FaArrowAltCircleRight)`color: #fff;`;
+  const lastPageButton = styled(FaArrowAltCircleLeft)`color: #fff;`;
+
+  const getMovieRequest = async (searchValue, page) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&page=${page}&type=movie&apikey=ddacb17c`
     const response = await fetch(url)
     const responseJson = await response.json();
 
     if (responseJson.Search) {
+      console.log(movies)
       setMovies(responseJson.Search)
     }
   }
 
   useEffect(() => {
-    getMovieRequest(searchValue);
+    getMovieRequest(searchValue, page);
   }, [searchValue]);
+
+
+
 
   return (
     <div className='container-fluid movie-app'>
@@ -31,10 +41,12 @@ const App = () => {
         <MovieListHeading heading="movies" />
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
-      <div className="row">
+      <div className="row2">
         <MovieList movies={movies} />
       </div>
+      <nextPageButton />
     </div>
+
   )
 }
 
--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import './MovieListStyle.css';
 
 const MovieList = (props) => {
   return (
     <>
-      {props.movies.map((movie, index) => (
-        <div className="d-flex justify-content-start m-3">
+      {/* {props.movies.forEach((movie, i) => (
+        <div className="poster d-flex justify-content-start m-3">
           <img src={movie.Poster} alt="movie"></img>
         </div>
-      ))}
+      ))
+      } */}
+      {props.movies.map((movie, index) => (
+        <div className="poster d-flex justify-content-start m-3">
+          <h1>{movie.Title}</h1>
+          <img src={movie.Poster} alt="Poster Not Found"></img>
+        </div>
+      ))
+      }
     </>
   );
 };
